993a5e160a28cb94e42175e34d4b08e82ee26717
Vaibhav <01vatri@gmail.com>
2020-01-30 23:31:12 +0530

    get current day and render day as string to list.ejs

--- app.js
+++ app.js
@@ -3,11 +3,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+let date = new Date();
+let curDay = date.getDay();
+let day="";
+switch (curDay) {
+  case 0: day = "Sunday"; break;
+  case 1: day = "Monday"; break;
+  case 2: day = "Tuesday"; break;
+  case 3: day = "Wednesday"; break;
+  case 4: day = "Thrusday"; break;
+  case 5: day = "Friday"; break;
+  case 6: day = "Saturday"; break;
+  default: day = "Some error occured"
+}
+
 app.get("/", function(req, res) {
-  res.send("Hello");
+  res.render("list", {Day: day});
 });
+
 app.listen(3000, function() {
   console.log("Server started to port 3000");
 })
