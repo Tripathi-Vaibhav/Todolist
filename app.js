8cc19f5516f30305b7437c44e39e8beeb94048e6
Vaibhav <01vatri@gmail.com>
2020-01-30 23:50:22 +0530

    change date format

--- app.js
+++ app.js
@@ -7,19 +7,14 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 let date = new Date();
-let curDay = date.getDay();
-let day="";
-switch (curDay) {
-  case 0: day = "Sunday"; break;
-  case 1: day = "Monday"; break;
-  case 2: day = "Tuesday"; break;
-  case 3: day = "Wednesday"; break;
-  case 4: day = "Thrusday"; break;
-  case 5: day = "Friday"; break;
-  case 6: day = "Saturday"; break;
-  default: day = "Some error occured"
+
+let options = {
+  day: "numeric",
+  weekday: "long",
+  month: "long"
 }
 
+let day = date.toLocaleString("en-US", options);
 app.get("/", function(req, res) {
   res.render("list", {Day: day});
 });
