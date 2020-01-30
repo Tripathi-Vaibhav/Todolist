f4a097d36164a26af12dca657b53d3d0ebbfdb50
Vaibhav <01vatri@gmail.com>
2020-01-31 01:10:20 +0530

    access to static css files by server

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 let items = ["Breakfast", "Lunch", "Dinner"];
 app.get("/", function(req, res) {
